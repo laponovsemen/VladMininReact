679f0d24eda6d9bc869b49e17aa538ed1d71197a
Laponov Semen <johndoe@example.com>
2023-12-13 01:44:59 +0400

    add error message component and loading component and custom Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Product} from "./components/Product";
-import {products} from "./data/products";
+import {useProducts} from "./hooks/products";
+import {Loader} from "./components/Loader";
+import {ErrorMessage} from "./components/ErrorMessage";
 
 
 function App() {
-  //const  [] = useState()
+  const {loading, error, products} = useProducts()
+
   return (
-  <div className="container mx-auto max-w-2xl pt-5">
-    <Product product={products[0]} />
+  <div className="container mx-auto max-w-2xl pt-10">
+    {loading && <Loader/>}
+    {error && <ErrorMessage error={error}/>}
+
+    {products.map(product =>
+        <Product product={product} key={product.id}/>
+    )}
+
+
+
+    {/*<Product product={products[0]} />
+    <Product product={products[1]} />*/}
 
   </div>
   );
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,13 +1,32 @@
 import {IProduct} from "../../models";
+import {useState} from "react";
 
 interface ProductProps {
 	product : IProduct
 }
 
-export function Product(props : ProductProps) {
+export function Product({product} : ProductProps) {
+	console.log(product.image)
+	const [details, setDetails] = useState(false)
+	const btnBGClassName = details ? "bg-blue-400" : "bg-amber-400"
+	const btnClasses = ["py-2 px-2 border", btnBGClassName]//
 	return (
 		<div className="border py-2 px-4 rounded flex flex-col  justify-center items-center mb-2">
-			{props.product.title}
+			<img src={product.image} className="w-1/6" alt={product.title}/>
+			<p>{product.title}</p>
+			<span className="font-bold">{product.price}</span>
+			<button
+				className={btnClasses.join(" ")}
+				onClick={() => setDetails(prev => !prev)}
+			>
+				{details ? "Hide details" : "Show details"}
+			</button>
+			{details &&
+				<div>
+					<p>{product.description}</p>
+					<p>Rate: <span style={{fontWeight: 'bold'}}>{product.rating.rate}</span></p>
+				</div>
+			}
 		</div>
 	)
 }
